593814dbf67d922b84227da138f7ffed6cb885e5
Tom Vieira <vieiran@email.sc.edu>
2018-11-16 09:42:37 -0500

    Fixed Health Bars Size
    Health Bars aren't too big any more

--- Assets/Scenes/src/HealthBar.cs
+++ Assets/Scenes/src/HealthBar.cs
@@ -10,7 +10,6 @@ public class HealthBar : MonoBehaviour {
 
     public void setHealth(float percentage)
     {
-        Debug.Log(percentage);
         if(innerHealthBar != null)
         {
             float newScale = percentage;
@@ -24,6 +23,7 @@ public class HealthBar : MonoBehaviour {
                 newScale = 0.1f;
             }
 
+            //scales healthbars as unit loses health
             innerHealthBar.transform.localScale = new Vector3 (newScale,innerHealthBar.transform.localScale.y,innerHealthBar.transform.localScale.z);
             innerHealthBar.transform.localPosition = new Vector3 ((newScale / 2.2f) - 0.4f, innerHealthBar.transform.localPosition.y, innerHealthBar.transform.localPosition.z);
         }
